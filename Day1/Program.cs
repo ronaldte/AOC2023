04ad1a5fbfc43281273276e5afc1f37374f0dcaa
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2024-02-08 22:13:22 +0100

    add day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+var sum = 0;
+
+foreach (string line in File.ReadLines(@"input.txt"))
+{
+    var wordToDigit = new Dictionary<string, int>()
+    {
+        {"zero", 0},
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9},
+        {"0", 0},
+        {"1", 1},
+        {"2", 2},
+        {"3", 3},
+        {"4", 4},
+        {"5", 5},
+        {"6", 6},
+        {"7", 7},
+        {"8", 8},
+        {"9", 9}
+    };
+
+    // Part1
+    //var pattern = @"(\d)";
+
+    // Part2
+    var pattern = @"(?=(?<number>\d|zero|one|two|three|four|five|six|seven|eight|nine))";
+
+    var matches = new Regex(pattern).Matches(line).Select(m => wordToDigit[m.Groups["number"].Value]);
+
+    sum += matches.First() * 10 + matches.Last();
+}
+
+Console.WriteLine(sum);
